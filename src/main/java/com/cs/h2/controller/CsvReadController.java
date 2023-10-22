3979d0efd8e41ceb0fbbbcca707d59e5b749e62c
tss <test@mail.com>
2023-10-22 16:16:24 +0530

    Updated Path of csv file

--- src/main/java/com/cs/h2/controller/CsvReadController.java
+++ src/main/java/com/cs/h2/controller/CsvReadController.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,9 @@ public class CsvReadController {
 	
 	@Autowired
 	private CsvService csvReadService;
+	
+	@Autowired
+	private ResourceLoader resourceLoader;
 
 	@GetMapping("/load")
 	public String test() throws FileNotFoundException, IOException {
@@ -37,11 +42,14 @@ public class CsvReadController {
 	// First Name ,Last Name,Designation,Contact,Sal,City
 
 	public void loadData() throws FileNotFoundException, IOException {
-		String path = "C:\\Users\\Admin\\temp\\Book1.csv";
+		//String path = "C:\\Users\\Admin\\temp\\Book1.csv";
+		
+		Resource classPathResource = resourceLoader.getResource("classpath:Book1.csv");
+		String rPath = classPathResource.getURI().getPath();
 
 		List<Employee> list = new ArrayList<>();
 
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+		try (BufferedReader br = new BufferedReader(new FileReader(rPath))) {
 			String line;
 			int i =0;
 			while ((line = br.readLine()) != null) {
