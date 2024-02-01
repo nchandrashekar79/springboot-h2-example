6ac7aad6f53e5f7a93793a1afb1ece7eeb6f3161
nchandrashekar79 <83388759+nchandrashekar79@users.noreply.github.com>
2024-02-01 20:51:09 +0530

    Update TableService.java

--- src/main/java/com/cs/h2/serviceimpl/TableService.java
+++ src/main/java/com/cs/h2/serviceimpl/TableService.java
@@ -1,3 +1,12 @@
+package com.cs.h2.serviceimpl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
 @Service
 public class TableService {
 
